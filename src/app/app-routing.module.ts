5e6299a21a495187cf1a5987a5fd9c303b53a869
Vidit Attri <vidit.attri@gmail.com>
2022-04-04 10:46:37 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {OktaAuthGuard, OktaCallbackComponent} from "@okta/okta-angular";
+import {ProfileComponent} from "./profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'profile', component: ProfileComponent, canActivate: [OktaAuthGuard] },
+
+  {
+    path: 'login/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'protected',
+    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+    canActivate: [OktaAuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  OktaAuthGuard,
+  OKTA_CONFIG,
+  OktaAuthModule
+} from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+
+
+
+
+const config = {
+  issuer: 'https://dev-4211287.okta.com/oauth2/default',
+  clientId: '0oa4i98bidEJPUSCk5d7',
+  redirectUri: window.location.origin + '/login/callback'
+}
+const oktaAuth = new OktaAuth(config);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {Routes} from "@angular/router";
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OktaAuthModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: OKTA_CONFIG,
+      useValue: { oktaAuth }
+    },
+    { provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
